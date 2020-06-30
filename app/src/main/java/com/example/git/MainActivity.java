31b1afcf584732774bcfa8d6c80c08fc5045cddf
pzb001025 <1275776996@qq.com>
2020-06-30 11:21:32 +0800

    首次

---
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.git;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String a = "a";
+        String b = "b";
+        String c = "c";
+    }
+}
